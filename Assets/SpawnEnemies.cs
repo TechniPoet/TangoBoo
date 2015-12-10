6a63a8548d4ca6d63887c6052db3cf65d835d295
Jeff Robbins <RobbinsJeffreyM@gmail.com>
2015-12-10 17:26:15 -0500

    Adding project as is

---
+++ Assets/SpawnEnemies.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+    public GameObject ghost;
+    bool found = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindObjectsOfType<ARLocationMarker>().Length > 0 && !found)
+        {
+            found = true;
+            ARLocationMarker marker = GameObject.FindObjectsOfType<ARLocationMarker>()[0];
+            StartCoroutine(wait());
+            GameObject newGhost = Instantiate(ghost,marker.transform.position,Quaternion.identity) as GameObject;
+            newGhost.SetActive(true);
+        }
+	}
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
